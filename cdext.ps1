6473dd5d37510458670eecf0a9c5de1645896036
Skyhunter2 <97682534+Skyhunter2@users.noreply.github.com>
2025-07-23 14:22:03 -0400

    Update cdext.ps1
    Added Save functionality

--- cdext.ps1
+++ cdext.ps1
@@ -3,40 +3,26 @@ param (
     $value
 )
 
-code $PROFILE.CurrentUserAllHosts
+#code $PROFILE.CurrentUserAllHosts
 
-#If does not already exist, creates a file to append functions too.
-if(!(Test-Path -Path ".\alias.ps1")) {
-    New-Item -Path . -Name "alias.ps1" -ItemType "File" #-Value "This is a text string."
-} else {
-    #import-Alias -Path ".\alias.ps1" -Force
+#If the profile does not already exist, creates a file to append functions too.
+if(!(Test-Path -Path $PROFILE.CurrentUserAllHosts)) {
+    New-Item -path $PROFILE -type File -Force
 }
-$AliasStorageFilePath = $PROFILE.CurrentUserAllHosts #Sets the created or existing file to contain the alias
-                                      #All alais functions will be sored here.
-                                      #Functions as a way to use the alias even outside the cdext tool
+
+$AliasStorageFilePath = $PROFILE.CurrentUserAllHosts #Stores the alias in the powershell profile 
 
 #todo: Implement this as a script for storing alias definitions, then call it in C# to store aliases 
 #todo: Needs to have the functions persists so that they can be permantly set as an alias.
 #Figure out the best way to do this. 
 
-#notepad.exe $HOME\Documents\PowerShell\Profile.ps1
-
-#Runs the CDEXT script
-function worldfunc {
-    Write-Host "Hello World"
-}
-#$name = cdextfunc
-
-Clear-Content -Path $AliasStorageFilePath
+#Clear-Content -Path $AliasStorageFilePath
 #Puts the script alias in the file
-Add-Content -Path $AliasStorageFilePath -Value "function worldfunc { Write-Host `"Hello World`" } `nSet-Alias -Name hello -value worldfunc -Scope Global"
-#Add-Content -Path $AliasStorageFilePath -Value "param (`n`$func`n)"
-#Add-Content -Path $AliasStorageFilePath -Value "if (`$func -eq 1) {write-host 'Success'}"
-#Add-Content -Path $AliasStorageFilePath -Value "Get-ChildItem"
+#Add-Content -Path $AliasStorageFilePath -Value "function worldfunc { Write-Host `"Hello World`" }" #`nSet-Alias -Name hello -value worldfunc -Scope Global"
 
 #Set-Alias -Name test -value .\alias.ps1 -Scope Global
 
-Set-Alias -Name hello -value worldfunc -Scope Global
+#Set-Alias -Name hello -value worldfunc -Scope Global
 
 #Creates a an alias for the function. 
 #Set-Alias -Name cdext -Value C:\Users\orion.newell\Documents\cdext.ps1 -Scope Global 
@@ -48,11 +34,11 @@ function New {
         $name,
         $value
     )
-    Write-Host "wip"
+    Set-Alias -Name $name -Value -$value
 }
 
 #The definition of the function
-function Aliasfunc() {
+function Aliasfunc {
     Write-Host $value
     cmd /c $value
 }
@@ -61,24 +47,90 @@ function Aliasfunc() {
 #Set-Alias -Name $name -Value Aliasfunc -Scope Global
 
 #Lists known alias'
-function List() {
+function List {
     param (
         $List
     )
     Write-Host "wip"
 }
 
+function Remove {
+    param (
+        $value,
+        $all
+    )
+    if($all) {
+        Clear-Content -Path $AliasStorageFilePath -Exclude "init"
+    }
+}
+
+function PrintFunctionAsString {
+    param (
+        [Parameter(Mandatory=$true)]$name
+    )
+
+    $FunctionObject = Get-Command -Name $name -CommandType Function
+    $FunctionString = $FunctionObject.ScriptBlock.ToString()
+    return "function $name { $FunctionString }"   
+}
+
+
+#Functions related to pathway management
+
+#Saves either current directory or user can name a specific pathway to save as
+function Save {
+    param (
+        [Parameter(Mandatory=$true)]$name,
+        $value
+    )
+    if($null -eq $value) {
+        Set-Alias -Name $name -Value $pwd
+    } else {
+        Set-Alias -Name $name -Value $value
+    } 
+}
+
+function cd_to {
+    param (
+        $path
+    )
+    Set-Location $path.ToString()
+}
+
 #intilializes program and sets the defualt alias to be referenced by the cdext script
+
+#Change powershell to be able to parse spaces properly
 function initialize() {
     $fileContent = Get-Content -Path $AliasStorageFilePath
-    if ($fileContent[0] -eq "init") {
+    if ($fileContent[1] -eq "#init") {
         #TODO: Develop better check system because this one is kinda bad :)
     } else {
         Write-Host "Initializing CDEXT"
-        Add-Content -Path $AliasStorageFilePath -Value "init" #puts a 'tag' in the profile to check if the program has been previously initialized.
-        Add-Content -Path $AliasStorageFilePath -Value "function worldfunc { Write-Host `"Hello World`" } `nSet-Alias -Name hello -value worldfunc -Scope Global"
-        ASet-Alias -Name cdext -Value $PWD\cdext.ps1 -Scope Global 
+        Add-Content -Path $AliasStorageFilePath -Value "#init" -Force #puts a 'tag' in the profile to check if the program has been previously initialized.
+
+        #
+        Add-Content -Path $AliasStorageFilePath -Value "Set-Alias -Name cdext -Value `$PWD\cdext.ps1 -Scope Global" 
+        #Add-Content -Path $AliasStorageFilePath -Value "Set-Alias -Name cd_to -Value `$PWD\cdext.ps1 -Scope Global" #Todo: Make powershell able to parse the space in between here 
+        Add-Content -Path $AliasStorageFilePath -Value  "function New {param (`$name,`$value) Set-Alias -Name `$name -Value -`$value}"
+        Add-Content -Path $AliasStorageFilePath -Value "Set-Alias -Name new -Value new"
+
+        #Remove Functionality
+        $RemoveStr = PrintFunctionAsString -name Remove
+        Add-Content -Path $AliasStorageFilePath -Value $RemoveStr
+        Set-Alias -Name "-remove" -Value Remove
+        Set-Alias -Name "-rem" -Value Remove
+        Set-Alias -Name "-R" -Value Remove
+
+        #save Functionality
+        $SaveStr = PrintFunctionAsString -name Save
+        Add-Content -Path $AliasStorageFilePath -Value $SaveStr
+        Set-Alias -Name -save -Value Save
+        Set-Alias -Name -sav -Value Save
+        Set-Alias -Name -sa -Value Save
+        Set-Alias -Name "-s" -Value Save
     }
 }
 
 initialize
+. $PROFILE.CurrentUserAllHosts
+notepad.exe $PROFILE.CurrentUserAllHosts
